049955a629dbcab20564a9fd2a54a9e41a47a153
devilHacker0672 <devilhacker0672@gmail.com>
2026-07-19 12:13:38 +0530

    Data Type in java or variable declaration

---
+++ data.java
@@ -0,0 +1,13 @@
+public class data {
+     static void main() {
+          byte num1 = 10;
+          short num2 = 20;
+          int num3 = 30;
+          long num4 = 40L;
+          System.out.println("Byte value: " + num1);
+          System.out.println("Short value: " + num2);
+          System.out.println("Int value: " + num3);
+          System.out.println("Long value: " + num4);
+          
+     }
+}
